2950503e44e7410d3b5818a97450f28543758307
GhislainFaure <ghislainfaure06@gmail.com>
2024-10-29 12:43:16 +0100

    mise en place de la vue city + utilisation du module dayjs

---
+++ my_modules/capitalCities.js
@@ -0,0 +1,24 @@
+const capitalCities = [
+  {
+    name: "Paris",
+    tz: "Europe/Paris",
+  },
+  {
+    name: "Londres",
+    tz: "Europe/London",
+  },
+  {
+    name: "Washington",
+    tz: "US/Eastern",
+  },
+  {
+    name: "Beijing",
+    tz: "Asia/Shanghai",
+  },
+  {
+    name: "Moscou",
+    tz: "Europe/Moscow",
+  },
+];
+
+module.exports = capitalCities;
